bb3e9b12dbf63daa9d42f1a818ffbb5113552964
Gizi <szanto.gizela@gmail.com>
2025-07-10 11:59:17 +0200

    update admin to users table

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
@@ -13,8 +14,16 @@ class AdminMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
+    // public function handle(Request $request, Closure $next): Response
+    // {
+    //     return $next($request);
+    // }
     public function handle(Request $request, Closure $next): Response
-    {
+{
+    if (Auth::check() && Auth::user()->is_admin){
         return $next($request);
     }
+    abort(403);
+}
+
 }
