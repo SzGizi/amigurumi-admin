983ecbedd70595c4fbd6157763eb0e906c045b38
Gizi <szanto.gizela@gmail.com>
2025-07-14 12:22:12 +0200

    feat: integrate SCSS support with Vite and Laravel
    - Added Sass dependency to the project
    - Created `resources/sass/app.scss` for custom styles
    - Imported SCSS into `app.js` to ensure it is compiled by Vite
    - Verified build process and hot module replacement (HMR) for SCSS works

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,8 @@ import './bootstrap';
 //import 'bootstrap';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Sortable from 'sortablejs';
+import '../sass/app.scss';
+
 import { createApp } from 'vue';
 import axios from 'axios';
 import AmigurumiPatternEdit from './components/AmigurumiPatternEdit.vue';
--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,15 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js','resources/sass/app.scss'],
             refresh: true,
         }),
         vue(),
     ],
      resolve: {
-        alias: { vue: 'vue/dist/vue.esm-bundler.js' },
+        alias: { 
+            vue: 'vue/dist/vue.esm-bundler.js' ,
+        
+        },
     },
 });
-
